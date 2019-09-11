15e9d392967ed7d4a57f273327d849d122360d58
tracer-ics2019 <tracer@njuics.org>
2019-09-11 19:59:16 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:59:16 up  3:26,  1 user,  load average: 0.44, 0.38, 0.25 b54303c44799ddc27e26df8a1481c0edc388d209

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -61,10 +61,10 @@ static int cmd_info(char *args) {
     printf("Function hasn't been realized yet.");
   }
   if (arg == NULL) {
-    printf("Input Error! You are expected to input 'r' or 'w' after 'info'/r/n");
+    printf("Input Error! You are expected to input 'r' or 'w' after 'info'/n/r");
   }
   else {
-    printf("Input Error! You are expected to input 'r' or 'w' after 'info'/r/n");
+    printf("Input Error! You are expected to input 'r' or 'w' after 'info'/n/r");
   } 
   return 0;
 } 
