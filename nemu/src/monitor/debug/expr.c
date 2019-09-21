d7d5e626e098d93410ad11dc4aa80af5acf8ffd1
tracer-ics2019 <tracer@njuics.org>
2019-09-21 11:23:42 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  11:23:42 up 27 min,  1 user,  load average: 0.00, 0.05, 0.11 bf3782bf8931ce3f4c2e64c0d43007ffbe9f35e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -173,7 +173,7 @@ uint32_t eval(int p, int q) {
       case '-': return val1 - val2;
       case '*': return val1 * val2;
       case '/': return val1 / val2;
-      default: assert(0);
+     // default: assert(0);
     }
   }
   return 0;
