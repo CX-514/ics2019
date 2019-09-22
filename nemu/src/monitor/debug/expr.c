29aec1f9a9bb3d25d8a6010afaaa41d5cc15f14d
tracer-ics2019 <tracer@njuics.org>
2019-09-22 16:32:36 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  16:32:36 up  2:42,  1 user,  load average: 0.23, 0.14, 0.05 4898be9a8d863b1c80ebe5f31b0abe17aea37924

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -91,9 +91,9 @@ static bool make_token(char *e) {
           case '-': tokens[nr_token].type = '-'; break;
           case '*': tokens[nr_token].type = '*'; break;
           case '/': tokens[nr_token].type = '/'; break;
-          case TK_EQ: tokens[nr_token].type = rules[i].token_type; strncpy(tokens[nr_token].str,substr_start,substr_len); break;
+          case TK_EQ: tokens[nr_token].type = rules[i].token_type; memset(tokens[nr_token].str, '\0',1000); strncpy(tokens[nr_token].str,substr_start,substr_len); break;
           case TK_UEQ: tokens[nr_token].type = rules[i].token_type; strncpy(tokens[nr_token].str,substr_start,substr_len); break;
-          case TK_TEN: tokens[nr_token].type = rules[i].token_type; strncpy(tokens[nr_token].str,substr_start,substr_len); break;
+          case TK_TEN: tokens[nr_token].type = rules[i].token_type; memset(tokens[nr_token].str, '\0',1000); strncpy(tokens[nr_token].str,substr_start,substr_len); break;
           case '(': tokens[nr_token].type = '('; break;
           case ')': tokens[nr_token].type = ')'; break;
           default: TODO();
