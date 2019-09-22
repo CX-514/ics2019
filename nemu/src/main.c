f7f15792ae7c62a2aae24e40eb343a10e941df68
tracer-ics2019 <tracer@njuics.org>
2019-09-22 14:43:25 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  14:43:25 up 53 min,  1 user,  load average: 0.04, 0.10, 0.17 5a0609b8379ac3b9b9707fdc3d1f92cc3067cc5

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -12,18 +12,16 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
-  char buf[1000];
+  char *buf = (char*)malloc(65536*sizeof(char));
   float count=0,suc=0;
   FILE *fp = fopen("/home/cx/ics2019/nemu/tools/gen-expr/input","r");
-  while(fgets(buf,1000,fp)) {
+  while(fgets(buf,65536,fp)) {
     char *arg1 = strtok(buf, " ");
     char *arg2 = strtok(NULL, "\n");
     bool success = true;
-    int n;
-    sscanf(arg1,"%d", &n);
     int result=expr(arg2,&success);
     if (success) {
-      if (result==n)
+      if (result==atoi(arg1))
         suc++;
     }
     count++; 
