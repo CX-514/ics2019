0818a33061f8d0275265c94055eea886f9b1ea12
tracer-ics2019 <tracer@njuics.org>
2019-09-22 16:35:29 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  16:35:29 up  2:45,  1 user,  load average: 0.12, 0.11, 0.05 2911a9e73913f23665969a9068e336bcd3116fe3

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
- /* char *buf = (char*)malloc(65536*sizeof(char));
+  char *buf = (char*)malloc(65536*sizeof(char));
   int suc=0;
   FILE *fp = fopen("/home/cx/ics2019/nemu/tools/gen-expr/input","r");
   while(fgets(buf,65536,fp)!=NULL) {
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
   }
   fclose(fp);
   float acc = suc/100.0;
-  printf("%f\n",acc);*/
+  printf("%f\n",acc);
   
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
