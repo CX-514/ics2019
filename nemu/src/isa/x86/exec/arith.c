7c3f02d7966e65b8e3b18a48524759d6e21f3e48
tracer-ics2019 <tracer@njuics.org>
2019-10-16 16:03:33 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  16:03:33 up 17 min,  1 user,  load average: 0.75, 0.56, 0.40 1f79e455b5b49b80bbe9eb806eeb9cb5b2210b72

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -10,19 +10,18 @@ make_EHelper(sub) {
   rtl_sub(&s0, &id_dest->val, &id_src->val);
   
   operand_write(id_dest, &s1);
+ 
   if (id_dest->width != 4) {
-    rtl_andi(&s1, &s1, 0xffffffffu >> ((4 - id_dest->width) * 8));
+    rtl_andi(&s0, &s0, 0xffffffffu >> ((4 - id_dest->width) * 8));
   }
 
-  rtl_update_ZFSF(&s1, id_dest->width);
+  rtl_update_ZFSF(&s0, id_dest->width);
 
-  rtl_is_sub_carry(&s1, &s1, &s0);
-  rtl_is_sub_carry(&s0, &s0, &id_dest->val);
-  rtl_or(&s0, &s0, &s1);
-  rtl_set_CF(&s0);
+  rtl_is_sub_carry(&s1, &s0, &id_dest->val);
+  rtl_set_CF(&s1);
 
-  rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
-  rtl_set_OF(&s0);
+  rtl_is_sub_overflow(&s1, &s0, &id_dest->val, &id_src->val, id_dest->width);
+  rtl_set_OF(&s1);
   print_asm_template2(sub);
 }
 
