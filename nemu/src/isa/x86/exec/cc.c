3c6893e5d1efc2c4696c536d095456124ce16af0
tracer-ics2019 <tracer@njuics.org>
2019-10-23 18:50:21 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  18:50:21 up 59 min,  1 user,  load average: 0.35, 0.31, 0.31 ea2169bb82abaf00bc167bfa796aedf17262ea3b

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -20,7 +20,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_BE: rtl_get_ZF(&s0); rtl_get_CF(&s1); *dest=s0||s1; break;
     case CC_S:  rtl_get_SF(&s0); *dest=s0; break;
     case CC_L:  rtl_get_SF(&s0); rtl_get_OF(&s1); *dest=(s0!=s1); break;
-    case CC_LE: rtl_get_SF(&s0); rtl_get_OF(&s1); rtl_get_ZF(&t1); *dest=(t1||(s0!=s1))?0:1; break;
+    case CC_LE: rtl_get_SF(&s0); rtl_get_OF(&s1); rtl_get_ZF(&t1); *dest=(t1||(s0!=s1))?1:0; break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
