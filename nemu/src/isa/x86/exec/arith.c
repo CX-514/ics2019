6b1c7e08945d6e6225544632e436a9e7ca549f83
tracer-ics2019 <tracer@njuics.org>
2019-10-23 10:14:37 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  10:14:37 up 28 min,  1 user,  load average: 0.74, 0.74, 0.66 3de2c48b3f332217a7726c83631b11206aeba49a

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -52,11 +52,8 @@ make_EHelper(cmp) {
   rtl_is_sub_carry(&s1, &s0, &id_dest->val);
   rtl_set_CF(&s1);
 
-  rtl_xor(&s0, &id_dest->val, &s0);
-	rtl_xor(&s1, &id_dest->val, &id_src->val);
-	rtl_and(&s0, &s1, &s0);
-  rtl_msb(&s0, &s0, id_dest->width);
-  rtl_set_OF(&s1);
+  rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);
+  rtl_set_OF(&s0);
   
 
   print_asm_template2(cmp);
