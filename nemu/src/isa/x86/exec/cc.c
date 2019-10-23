8aad20a8fb9e36b0913e39d97cbf1db06ed8fa72
tracer-ics2019 <tracer@njuics.org>
2019-10-23 18:52:58 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  18:52:58 up  1:02,  1 user,  load average: 0.25, 0.27, 0.29 c1a7a1a43a29c7c7670fd35f9c8bfeac9e6f8729

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -14,12 +14,12 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:  rtl_get_OF(&s0); *dest=s0; break;
-    case CC_B:  rtl_get_CF(&s0); *dest=s0; break;
-    case CC_E:  rtl_get_ZF(&s0); *dest=s0; break;
-    case CC_BE: rtl_get_ZF(&s0); rtl_get_CF(&s1); *dest=s0||s1; break;
+    case CC_O:  rtl_get_OF(&s0); *dest=s0?1:0; break;
+    case CC_B:  rtl_get_CF(&s0); *dest=s0?1:0; break;
+    case CC_E:  rtl_get_ZF(&s0); *dest=s0?1:0; break;
+    case CC_BE: rtl_get_ZF(&s0); rtl_get_CF(&s1); *dest=(s0||s1)?1:0; break;
     case CC_S:  rtl_get_SF(&s0); *dest=s0; break;
-    case CC_L:  rtl_get_SF(&s0); rtl_get_OF(&s1); *dest=(s0!=s1); break;
+    case CC_L:  rtl_get_SF(&s0); rtl_get_OF(&s1); *dest=(s0!=s1)?1:0; break;
     case CC_LE: rtl_get_SF(&s0); rtl_get_OF(&s1); rtl_get_ZF(&t1); *dest=(t1||(s0!=s1))?1:0; break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
