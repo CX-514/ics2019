c0b5e6499c63f9b9c0673f86fdb5df01b21919d3
tracer-ics2019 <tracer@njuics.org>
2019-10-27 01:38:50 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  01:38:50 up  1:11,  1 user,  load average: 0.00, 0.04, 0.07 8cf603781e308edd165c2a7ef4e2a20e1979e72f

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -68,7 +68,7 @@ void* memcpy(void* out, const void* in, size_t n) {
 int memcmp(const void* s1, const void* s2, size_t n){
   const unsigned char *p1=s1,*p2=s2;
   while(n--) 
-    if(*p1!=*p2)
+    if(*p1 != *p2)
       return *p1-*p2;
     else
       p1++,p2++;
