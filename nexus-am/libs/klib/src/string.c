949a69c5a2778e55fc120f228445eabbb74706b3
tracer-ics2019 <tracer@njuics.org>
2019-10-27 02:16:46 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  02:16:46 up  1:49,  1 user,  load average: 0.31, 0.17, 0.11 a6ec4cb535fc456c8cdd6d3d9b593cd5f2f1a21

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -10,7 +10,7 @@ size_t strlen(const char *s) {
 
 char *strcpy(char* dst,const char* src) {
   char* tmp=dst;
-  while(-1);
+  while(1);
   return tmp;
   /*size_t i;
   for(i=0;src[i]!='\0';i++) {
