2520dbbc7625638471dc54806fc45f8b94a4ac0a
tracer-ics2019 <tracer@njuics.org>
2019-10-27 00:51:57 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  00:51:57 up 24 min,  1 user,  load average: 0.08, 0.11, 0.18 47aac176819eeb3761384793c90cf587eb7246e

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,81 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t len;
+  for(len=0; s[len]!='\0';len++);
+  return len;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  size_t i;
+  for(i=0;src[1]!='\0';i++) {
+    dst[i]=src[i];
+  }
+  dst[i]='\0';
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  size_t i;
+  for(i=0;i<n && src[i]!='\0';i++) {
+    dst[i]=src[i];
+  }
+  for(;i<n;i++) {
+    dst[i]='\0';
+  }
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  size_t i,j;
+  i=strlen(dst);
+  for(j=0;src[j]!='\0';j++) {
+    dst[i+j]=src[j];
+  }
+  dst[i+j]='\0';
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  while(*s1 && (*s1==*s2)) {
+    s1++;
+    s2++;
+  }
+  return *s1-*s2;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+  while(n--) {
+    if(*s1++!=*s2++)
+      return *s1-*s2;
+  }
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  unsigned char* p=v;
+  while(n--)
+    *p++=(unsigned char)c;
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  char *dp=out;
+  const char *sp=in;
+  while(n--) {
+    *dp++=*sp++;
+  }    
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  const unsigned char *p1=s1,*p2=s2;
+  while(n--) {
+    if(*p1!=*p2)
+      return *p1-*p2;
+    else
+      p1++,p2++;
+  }
   return 0;
 }
 
