c8b27779944e74c8daa41b12af66ab564494d3da
tracer-ics2019 <tracer@njuics.org>
2019-10-28 00:31:09 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  00:31:09 up  2:44,  1 user,  load average: 0.20, 0.14, 0.10 11a61ef70a2ed30f40a9ccf28be7433ede5e826

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -12,7 +12,61 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap,fmt);
+  int i=0;
+  while(*fmt!='\0') {
+    if(*fmt=='%') {
+      fmt++;
+      switch(*fmt) {
+        case 'd': {
+	  fmt++;
+	  int j=va_arg(ap,int);
+	  char ls[50]="";
+  	  int m,s;
+	  m=0;
+	  if(j>0) {
+ 	    while(j>0) {
+	      ls[m++]=(j%10)+'0';
+	      j=j/10;
+	    }
+	    s=0;
+	  }
+	  else if(j==0) {ls[m++]='0';s=0;}
+	  else if(j<0) {
+	     while(j<0) {
+                ls[m++]=(-(j%10))+'0';
+                j=j/10;
+             }
+             s=1;
+	  }
+	  if(s==1) {*out='-';out++;i++;}
+	  for(int n=m-1; n>=0;n--) {
+	    *out=ls[n];
+ 	    i++;
+	    out++;
+	  }break;
+        }
+	case 's': {
+	  fmt++;
+	  char *src=va_arg(ap,char*);
+	  while(*src!='\0') {
+	    *out=*src;
+	    i++;
+	    out++;
+	  }break;
+	}
+      }
+    }
+    else {
+      *out=*fmt;
+      i++;
+      out++;
+      fmt++;
+    }
+  }
+  *out='\0';
+  return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
