c1d78455cd8c1275629b44e574ee6491238f7f5e
tracer-ics2019 <tracer@njuics.org>
2019-10-27 16:22:42 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  16:22:42 up  1:11,  1 user,  load average: 0.32, 0.21, 0.20 24f9b46b7b6a81ead4e9193e4c069be78cbcd346

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -63,7 +63,8 @@ make_EHelper(inc) {
 
   rtl_update_ZFSF(&s0,id_dest->width);
 
-  rtl_setrelop(RELOP_LTU,&s1,&id_dest->val,&s0);
+  rtl_xor(&s1,&id_dest->val,&s0);
+  rtl_msb(&s1,&s1,id_dest->width);
   rtl_set_OF(&s1);  
 
   print_asm_template1(inc);
@@ -75,7 +76,8 @@ make_EHelper(dec) {
 
   rtl_update_ZFSF(&s0,id_dest->width);
   
-  rtl_setrelop(RELOP_LTU,&s1,&id_dest->val,&s0);
+  rtl_xor(&s1,&id_dest->val,&s0);
+  rtl_msb(&s1,&s1,id_dest->width);
   rtl_set_OF(&s1);
 
   print_asm_template1(dec);
