5f2ae0ae718abf24ae5ef6849a7d394b054bc8d8
tracer-ics2019 <tracer@njuics.org>
2019-10-28 18:26:45 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  18:26:45 up 9 min,  1 user,  load average: 0.27, 0.41, 0.33 fe554e346a2268b58f8d195c40531c525430a82

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,48 +3,6 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-char* my_itoa(char* dest, int n, int base){
-	/*transfer int into char[]*/
-	//int outn = n;
-	char number[20];
-	for(int i = 0; i <= 9; ++i) number[i] = i + '0';
-	number[10] = 'a'; number[11] = 'b'; number[12] = 'c';
-	number[13] = 'd'; number[14] = 'e'; number[15] = 'f';
-	if(n == 0) {
-		dest[0] = '0';
-		dest[1] = '\0';
-		return dest;
-	}
-	
-	int len = 0;
-	int neg = 0;
-	if(n < 0) {
-		neg = 1;
-		len ++;
-		n = -n;
-	}
-	int temp = n;
-	while(temp > 0){
-		len ++;
-		temp /= base;
-	}
-	
-	for(int i = 0; i < len; ++i){
-		int x = n % base;
-		assert(x < 16);
-		dest[len - i - 1] = number[x];
-		//printf("dest[%d] = %c\n", len-i-1,dest[len-i-1]);
-		n /= base;
-	}
-	if(neg) dest[0] = '-';
-	dest[len] = '\0';
-	//printf("%d -> %s\n",outn,dest);
-	return dest;
-}
-
-int printf(const char *fmt, ...) {
-  return 0;
-}
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
@@ -107,45 +65,7 @@ int sprintf(char *out, const char *fmt, ...) {
   *out='\0';
   va_end(ap);
   return i; */
-  //return 0;
-  int len = strlen(fmt);
-	int i = 0;
-	int in_size = 0;
-	va_list var_arg;
-	va_start(var_arg, fmt);
-	out[0] = '\0';
-
-	while(i < len){
-		if(fmt[i] == '%'){
-			int in_num = 0;
-			char in_char[50] = "";
-			switch(fmt[i+1]){
-				case 'd'://%d 
-					in_num = va_arg(var_arg,int);
-					my_itoa(in_char, in_num, 10);
-					strcat(out, in_char);
-					in_size += strlen(in_char);
-					break;
-				case 's'://%s
-				 	strcpy(in_char,va_arg(var_arg, char*));
-					strcat(out,in_char);
-					in_size += strlen(in_char);
-					break;
-				default: printf("Not implememted argument type!\n");
-			}
-			i += 2;
-		}
-		else {
-			char temp[2];
-			temp[0] = fmt[i];
-			temp[1] = '\0';
-			strcat(out, temp);
-			in_size ++;
-			i ++;
-		}
-	}
-		va_end(var_arg);
-return in_size;
+  return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
