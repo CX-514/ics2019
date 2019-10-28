67a1b330460098e3191ad30245d99943105b2279
tracer-ics2019 <tracer@njuics.org>
2019-10-28 20:59:29 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:59:29 up 22 min,  1 user,  load average: 0.65, 0.40, 0.37 b0c08cda70f3ef7c972897ef7d693361267dc9f9

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -28,6 +28,7 @@ int sprintf(char *out, const char *fmt, ...) {
 	  		num=va_arg(ap,int);
 	  		char number[20];
 	  		for (int j=0;j<10;j++) {number[j]=j+'0';}
+			number[10] = 'a'; number[11] = 'b'; number[12] = 'c'; number[13] = 'd'; number[14] = 'e'; number[15] = 'f';
 	  		int l=0;
 	  		int neg=0;
 			if(num==0) {
