ba4f23f0492daf6d629dc572bf8bb21e3bf79638
tracer-ics2019 <tracer@njuics.org>
2019-10-28 20:45:02 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:45:02 up 8 min,  1 user,  load average: 0.44, 0.56, 0.37 6f3385afa017c5e36ac5bd1b2ba2cbe4bb1c194b

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,69 +3,75 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int printf(const char *fmt, ...) {
+	return 0;
+}
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   return 0;
 }
 
+
 int sprintf(char *out, const char *fmt, ...) {
-/*  va_list ap;
+  va_list ap;
   va_start(ap,fmt);
+  int sum=0;
   int i=0;
-  while(*fmt!='\0') {
-    if(*fmt=='%') {
-      fmt++;
-      switch(*fmt) {
+  int len=strlen(fmt);
+  while(i<len) {
+    if(fmt[i]=='%') {
+      int num=0;
+      char ls[50]="";
+      switch(fmt[i+1]) {
         case 'd': {
-	  fmt++;
-	  int j=va_arg(ap,int);
-	  char ls[50]="";
-  	  int m,s;
-	  m=0;
-	  if(j>0) {
- 	    while(j>0) {
-	      ls[m++]=(j%10)+'0';
-	      j=j/10;
-	    }
-	    s=0;
-	  }
-	  else if(j==0) {ls[m++]='0';s=0;}
-	  else if(j<0) {
-	     while(j<0) {
-                ls[m++]=(-(j%10))+'0';
-                j=j/10;
-             }
-             s=1;
-	  }
-	  if(s==1) {*out='-';out++;i++;}
-	  for(int n=m-1; n>=0;n--) {
-	    *out=ls[n];
- 	    i++;
-	    out++;
-	  }break;
-        }
-	case 's': {
-	  fmt++;
-	  char *src=va_arg(ap,char*);
-	  while(*src!='\0') {
-	    *out=*src;
-	    i++;
-	    out++;
-	  }break;
+	  		num=va_arg(ap,int);
+	  		char number[10];
+	  		for (int j=0;j<10;j++) number[i]=i+'0'; 
+	  		if(num==0) {
+	    		ls[0]='0';
+	    		ls[1]='\0';
+	  		}
+ 	  		int l=0;
+	  		int neg=0;
+	  		if(num<0) {
+	    		neg=1;
+	    		l++;
+	    		num=-num;
+	  		}
+	  		int temp=num;
+	  		while(temp>0) {
+	    		l++;
+	    		temp=temp/10;
+	  		}
+	  		for (int a=0; a<l; a++) {
+	    		int x=num%10;
+	    		ls[l-a-1]=number[x];
+	    		num=num/10;
+	  		}
+	  		if(neg) ls[0]='-';
+	  		ls[l]='\0';
+	  		strcat(out,ls);
+	  		sum+=strlen(ls);	
+        } break;
+		case 's': {
+	  		strcpy(ls,va_arg(ap, char*));
+	  		strcat(out,ls);
+	  		sum += strlen(ls);
+		}break;
+	  } 
+    	i+=2;
 	}
-      }
-    }
     else {
-      *out=*fmt;
-      i++;
-      out++;
-      fmt++;
-    }
-  }
-  *out='\0';
+      	sum++;
+      	char s[2];
+      	s[0]=fmt[i];
+      	s[1]='\0';
+      	strcat(out,s);
+      	i++;
+  	}
+ }
   va_end(ap);
-  return i; */
-  return 0;
+  return sum; 
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
