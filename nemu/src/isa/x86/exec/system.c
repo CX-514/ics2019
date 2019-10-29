a2bd0dac8b3f7da17318846101f81748397d137b
tracer-ics2019 <tracer@njuics.org>
2019-10-29 20:44:41 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:44:41 up  1:22,  1 user,  load average: 0.68, 0.58, 0.32 8ce3b0cb47a9aff25f94acbfa3f73289563e1ec1

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -43,9 +43,15 @@ void pio_write_b(ioaddr_t, uint32_t);
 
 make_EHelper(in) {
   switch(id_dest->width) {
-    case 4: rtl_li(&s0, pio_read_l(id_src->val)); break;
-    case 2: rtl_li(&s0, pio_read_w(id_src->val)); break;
-    case 1: rtl_li(&s0, pio_read_b(id_src->val)); break;
+    case 4: 
+      rtl_li(&s0, pio_read_l(id_src->val)); 
+      break;
+    case 2: 
+      rtl_li(&s0, pio_read_w(id_src->val)); 
+      break;
+    case 1: 
+      rtl_li(&s0, pio_read_b(id_src->val)); 
+      break;
     default: assert(0);
   }
   operand_write(id_dest, &s0);
@@ -56,9 +62,15 @@ make_EHelper(in) {
 
 make_EHelper(out) {
   switch(id_dest->width) {
-    case 4: pio_write_l(id_dest->val, id_src->val); break;
-    case 2: pio_write_w(id_dest->val, id_src->val); break;
-    case 1: pio_write_b(id_dest->val, id_src->val); break;
+    case 4: 
+      pio_write_l(id_dest->val, id_src->val); 
+      break;
+    case 2: 
+      pio_write_w(id_dest->val, id_src->val); 
+      break;
+    case 1: 
+      pio_write_b(id_dest->val, id_src->val); 
+      break;
     default: assert(0);
   }
   print_asm_template2(out);
