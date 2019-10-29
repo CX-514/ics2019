3c41528df4d241d93b90858649bb7bf3fb8996dd
tracer-ics2019 <tracer@njuics.org>
2019-10-29 19:36:33 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:36:33 up 14 min,  1 user,  load average: 0.24, 0.23, 0.22 563107b0a11f964581986f658e7f4aa7acda0f11

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -46,7 +46,7 @@ make_EHelper(in) {
     case 4: rtl_li(&s0,pio_read_l(id_src->val)); break;
     case 2: rtl_li(&s0,pio_read_w(id_src->val)); break;
     case 1: rtl_li(&s0,pio_read_b(id_src->val)); break;
-    default: printf("in failed");
+    default: printf("in failed"); break;
   }
   operand_write(id_dest, &s0);
   
@@ -62,7 +62,7 @@ make_EHelper(out) {
     case 4: pio_write_l(id_dest->val, id_src->val); break;
     case 2: pio_write_w(id_dest->val, id_src->val); break;
     case 1: pio_write_b(id_dest->val, id_src->val); break;
-    default: printf("out failed");
+    default: printf("out failed"); break;
   }
 
   print_asm_template2(out);
