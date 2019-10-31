2858a1146d0e0f6bf772d734db40c5bde3a0aec7
tracer-ics2019 <tracer@njuics.org>
2019-10-31 16:23:02 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  16:23:02 up 7 min,  1 user,  load average: 0.23, 0.35, 0.21 149dd0036bee22d52865876738862e65ec3f8ab

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -36,7 +36,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
-      return sizeof(_DEV_VIDEO_FBCTL_t);
+      return size;
     }
   }
   return 0;
