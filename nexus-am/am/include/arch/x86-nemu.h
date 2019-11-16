9f0a8b38c809dc8290b6575524e8611c963695b1
tracer-ics2019 <tracer@njuics.org>
2019-11-16 17:38:39 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  17:38:39 up  3:04,  1 user,  load average: 0.51, 0.31, 0.20 7f6e153c6fdb5c704a4c20e9c66a67c9f74c63f

--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -2,9 +2,13 @@
 #define __ARCH_H__
 
 struct _Context {
-  uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
+  /*uintptr_t esi, ebx, eax, eip, edx, eflags, ecx, cs, esp, edi, ebp;
   struct _AddressSpace *as;
-  int irq;
+  int irq;*/
+  struct _AddressSpace *as;
+  uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  uint32_t irq;
+  uint32_t eip, cs, eflags;
 };
 
 #define GPR1 eax
