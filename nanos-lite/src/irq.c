90fa1881a8be8d5491c4afeb6956162fc1ab63c0
tracer-ics2019 <tracer@njuics.org>
2019-11-20 23:48:15 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  23:48:15 up  2:02,  1 user,  load average: 0.36, 0.29, 0.27 558695de7e11c345df24dd377a24e867e5b27ac

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,7 @@
 #include "common.h"
+extern _Context* do_syscall(_Context *c);
 
 static _Context* do_event(_Event e, _Context* c) {
-  extern _Context* do_syscall(_Context *c);
   switch (e.event) {
     case _EVENT_YIELD:{
       printf("yield!\n");
