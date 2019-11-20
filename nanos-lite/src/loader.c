305b4831ab4aa24e45f9c18ddab63ad46d767d31
tracer-ics2019 <tracer@njuics.org>
2019-11-20 22:09:53 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:09:53 up 23 min,  1 user,  load average: 0.77, 0.48, 0.33 602e0554a0c46989758874454d730fa7351d884f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,17 +12,19 @@
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr ehdr;
-  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*ehdr.e_phnum);
-  for (size_t i = 0; i < ehdr.e_phnum; i++) {
-    if (phdr[i].p_type == PT_LOAD) {
-      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-      memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
-    }
-  }
-  return ehdr.e_entry;
+  // Elf_Ehdr ehdr;
+  // ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  // Elf_Phdr phdr[ehdr.e_phnum];
+  // ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  // for (size_t i = 0; i < ehdr.e_phnum; i++) {
+  //   if (phdr[i].p_type == PT_LOAD) {
+  //     ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+  //     memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+  //   }
+  // }
+  // return ehdr.e_entry;
+  TODO();
+  return 0;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
