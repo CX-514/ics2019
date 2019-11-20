4607744df192bd9bc90f85d005bf51a341505adb
tracer-ics2019 <tracer@njuics.org>
2019-11-20 19:39:09 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  19:39:09 up  1:57,  1 user,  load average: 0.37, 0.44, 0.41 19676653748c5db650bf96a064a2a1c7be11a1e2

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -23,7 +23,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //   }
   // }
   Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read((void *)&phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*ehdr.e_phnum);
   for (size_t i = 0; i < ehdr.e_phnum; i++) {
     if (phdr[i].p_type == PT_LOAD) {
       ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
