412ea368b3024bb54cbfd811107a69ae600ed14b
tracer-ics2019 <tracer@njuics.org>
2019-11-20 20:02:26 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  20:02:26 up  2:20,  1 user,  load average: 0.56, 0.60, 0.52 45053b4bfec4b859269431b06141b3d91acc90c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,7 +9,7 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
