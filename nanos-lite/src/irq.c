30663087c25a6b23fa6d540feba883799492b5f4
tracer-ics2019 <tracer@njuics.org>
2019-11-20 23:46:26 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  23:46:26 up  2:00,  1 user,  load average: 0.21, 0.30, 0.27 c8f3d922f0f90bef5e151234e58ac48adf5be17b

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,14 +1,14 @@
 #include "common.h"
-extern _Context* do_syscall(_Context *c);
 
 static _Context* do_event(_Event e, _Context* c) {
+  extern _Context* do_syscall(_Context *c);
   switch (e.event) {
     case _EVENT_YIELD:{
       printf("yield!\n");
     } break;
     case _EVENT_SYSCALL:{
       return do_syscall(c);
-    } 
+    } break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
