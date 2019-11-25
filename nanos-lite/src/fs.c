a7ba877efb4751a6c91c147a23aa2b175a665821
tracer-ics2019 <tracer@njuics.org>
2019-11-25 15:40:46 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  15:40:46 up 8 min,  1 user,  load average: 0.82, 0.89, 0.56 dad9f918b366765320dab4fe20d7f40125f47c02

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,7 @@
 #include "fs.h"
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
@@ -7,6 +9,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  //size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -36,3 +39,34 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+// int fs_open(const char *pathname, int flags, int mode) {
+//   for (int i = 0; i < NR_FILES; i++) {
+//     if (strcmp(pathname, file_table[i].name) == 0) {
+//       file_table[i].open_offset = 1;
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+
+// size_t fs_read(int fd, void *buf, size_t len) {
+//   size_t res;
+//   size_t size;
+// 	if(file_table[fd].size && file_table[fd].open_offset + len > file_table[fd].size){
+// 		size = file_table[fd].size - file_table[fd].open_offset;
+// 	}
+//   if(file_table[fd].read == NULL){
+//   	res = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, size);
+// 	}
+// 	else {
+//     res = file_table[fd].read(buf, file_table[fd].open_offset, size);
+//   }
+//   file_table[fd].open_offset += res;
+//   return res;
+// }
+
+// int fs_close(int fd) {
+//   file_table[fd].open_offset = 0;
+//   return 0;
+// }
