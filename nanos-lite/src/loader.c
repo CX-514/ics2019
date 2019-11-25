c18fa7e213e6447716ce23199dc9583e102af84e
tracer-ics2019 <tracer@njuics.org>
2019-11-26 01:39:08 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  01:39:08 up  3:06,  1 user,  load average: 0.27, 0.41, 0.36 e915f98882746f0c9b31300bd0dfbdc7bc3ac06c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -33,22 +33,30 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   if (fd == -1) {
     panic("loader: can't open file %s!", filename);
   }
-
   Elf_Ehdr elf_header;
   fs_read(fd, (void *)&elf_header, sizeof(Elf_Ehdr));
   if (memcmp(elf_header.e_ident, ELFMAG, SELFMAG))
     panic("file %s ELF format error!", filename);
-
-  for (size_t i = 0; i < elf_header.e_phnum; ++i) {
-    Elf_Phdr phdr;
-    fs_lseek(fd, elf_header.e_phoff + elf_header.e_phentsize * i, SEEK_SET);
-    fs_read(fd, (void *)&phdr, elf_header.e_phentsize);
-    if (phdr.p_type == PT_LOAD) {
-      fs_lseek(fd, phdr.p_offset, SEEK_SET);
-      fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
-      memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+  // for (size_t i = 0; i < elf_header.e_phnum; ++i) {
+  //   Elf_Phdr phdr;
+  //   fs_lseek(fd, elf_header.e_phoff + elf_header.e_phentsize * i, SEEK_SET);
+  //   fs_read(fd, (void *)&phdr, elf_header.e_phentsize);
+  //   if (phdr.p_type == PT_LOAD) {
+  //     fs_lseek(fd, phdr.p_offset, SEEK_SET);
+  //     fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
+  //     memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
+  //   }
+  Elf_Phdr phdr[elf_header.e_phnum];
+  for (size_t i = 0; i < elf_header.e_phnum; i++) {
+    fs_lseek(fd, elf_header.e_phoff+i*elf_header.e_phentsize, SEEK_SET);
+    fs_read(fd, (void *)&phdr[i], elf_header.e_phentsize);
+    if (phdr[i].p_type == PT_LOAD) {
+      fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
+      fs_read(fd, (void *)&phdr[i].p_vaddr, phdr[i].p_filesz);
+      memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
+  
 
   fs_close(fd);
 
