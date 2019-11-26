eebb149beb6d6ce70049ebd7a0faafcb793bfd15
tracer-ics2019 <tracer@njuics.org>
2019-11-26 17:55:09 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  17:55:09 up 4 min,  1 user,  load average: 0.88, 0.76, 0.37 855a9cd6ca6421be5f776f30857c1b768b24274c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -19,14 +19,24 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   }
   Elf_Ehdr ehdr;
   fs_read(fd, (void *)&ehdr, sizeof(Elf_Ehdr));
-  Elf_Phdr phdr[ehdr.e_phnum];
-  for (size_t i = 0; i < ehdr.e_phnum; i++) {
-    fs_lseek(fd, ehdr.e_phoff+i*ehdr.e_phentsize, SEEK_SET);
-    fs_read(fd, (void *)&phdr[i], ehdr.e_phentsize);
-    if (phdr[i].p_type == PT_LOAD) {
-      fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
-      fs_read(fd, (void *)&phdr[i].p_vaddr, phdr[i].p_filesz);
-      memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+  // Elf_Phdr phdr[ehdr.e_phnum];
+  // for (size_t i = 0; i < ehdr.e_phnum; i++) {
+  //   fs_lseek(fd, ehdr.e_phoff+i*ehdr.e_phentsize, SEEK_SET);
+  //   fs_read(fd, (void *)&phdr[i], ehdr.e_phentsize);
+  //   if (phdr[i].p_type == PT_LOAD) {
+  //     fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
+  //     fs_read(fd, (void *)&phdr[i].p_vaddr, phdr[i].p_filesz);
+  //     memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+  //   }
+  // }
+  for (size_t i = 0; i < ehdr.e_phnum; ++i) {
+    Elf_Phdr phdr;
+    fs_lseek(fd, ehdr.e_phoff + ehdr.e_phentsize * i, SEEK_SET);
+    fs_read(fd, (void *)&phdr, ehdr.e_phentsize);
+    if (phdr.p_type == PT_LOAD) {
+      fs_lseek(fd, phdr.p_offset, SEEK_SET);
+      fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
+      memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
     }
   }
   fs_close(fd);
