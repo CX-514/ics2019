fa27cca56eceb3f882a7d97c8e594692ea14028b
tracer-ics2019 <tracer@njuics.org>
2019-11-27 22:02:03 +0800

    >  compile 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:02:03 up  1:20,  1 user,  load average: 0.37, 0.30, 0.26 f35d2eadff381031d465e249593fdd85fa98e78f

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -36,8 +36,8 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdout", 0, 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, 0, invalid_read, serial_write},
   {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
-#include "files.h"
   {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
+#include "files.h"
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
