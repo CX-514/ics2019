0284b6eab3d3c1cce0137152135373125290b5f4
tracer-ics2019 <tracer@njuics.org>
2019-11-27 22:26:04 +0800

    >  run 181220005 cx Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5 (2019-06-19) x86_64 GNU/Linux  22:26:04 up  1:44,  1 user,  load average: 0.39, 0.31, 0.46 ddd5d3fad2c545e0b1dfeff66d3cb9c4719165f3

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -5,6 +5,7 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t serial_write(const void *buf, size_t offset, size_t len);
 size_t fb_write(const void *buf, size_t offset, size_t len);
 size_t dispinfo_read(void *buf, size_t offset, size_t len);
+size_t fbsync_write(const void *buf, size_t offset, size_t len);
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
@@ -38,6 +39,7 @@ static Finfo file_table[] __attribute__((used)) = {
   {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
 #include "files.h"
   {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
+  {"/dev/fbsync", 0, 0, 0, invalid_read, fbsync_write},
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
